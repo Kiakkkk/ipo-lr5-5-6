1e0ff2009d2aa902455f50c948c74004a5a8a040
Kira Kisel <kira.kisel2008@gmail.com>
2024-10-19 14:52:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-file = open("text.txt", "r") #открывается файл для чтения
+with open("text.txt", "r") as file: #открывается файл для чтения
 s = file.read() #массив = текст из файла
 s = s.replace('o', 'a') #о заменяются на а
 with open("output.txt", "w+") as file: #output.txt открывает файл с возможностью изменения

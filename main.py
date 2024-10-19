2cdebfda476aab9dde752a45f92907273c1c2d57
Kira Kisel <kira.kisel2008@gmail.com>
2024-10-19 14:58:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 with open("text.txt", "r") as file: #открывается файл для чтения
-s = file.read() #массив = текст из файла
+    s = file.read() #массив = текст из файла
 s = s.replace('o', 'a') #о заменяются на а
 with open("output.txt", "w+") as file: #output.txt открывает файл с возможностью изменения
     file.write(s) #в output.txt записывается изменённая строка 
